ac08aecd31aed0fcf46d4e46a73f029f62735587
Vishnu Elayi <vishnuelayi@gmail.com>
2024-01-12 14:26:03 +0530

    implemented BlogList and Enquiry Pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
+import Enquiries from "./pages/Enquiries";
 import Login from "./pages/Login";
 import ForgotPassword from "./pages/ForgotPassword";
 import ResetPassword from "./pages/ResetPassword";
 import MainLayout from "./components/MainLayout";
+import BlogList from "./pages/BlogList";
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
         <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/admin" element={<MainLayout />}>
           <Route index element={<Dashboard />} />
+          <Route path="enquiries" element={<Enquiries />} />
+          <Route path="blog-list" element={<BlogList />} />
         </Route>
       </Routes>
     </Router>
