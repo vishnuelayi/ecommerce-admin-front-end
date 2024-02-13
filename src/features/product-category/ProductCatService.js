998f0e2d204a0b019f4e745b1ed92497b8c6386c
Vishnu Elayi <vishnuelayi@gmail.com>
2024-02-13 14:34:36 +0530

    created the addCategory method in product-categroy folder in features and implemented add category page

--- src/features/product-category/ProductCatService.js
+++ src/features/product-category/ProductCatService.js
@@ -34,6 +34,39 @@ const getProCats = async () => {
   }
 };
 
-const ProductCatService = { getProCats };
+const addCategory = async (data) => {
+  try {
+    // Retrieve the user object from local storage
+    const user = JSON.parse(localStorage.getItem("user"));
+
+    if (!user || !user.token) {
+      // Handle the case where there's no user object or no token property
+      console.error("No user object or token found");
+      // You might want to redirect the user to the login page or handle this case differently
+      return null;
+    }
+
+    const { token } = user;
+
+    const response = await axios.post(`${base_url}category/`,data, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (response.data) {
+      console.log("Added Category:", response.data);
+      // If necessary, store data in Redux store or handle it accordingly
+      // dispatch(setUsers(response.data));
+    }
+
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching users:", error.message);
+    throw error;
+  }
+};
+
+const ProductCatService = { getProCats, addCategory };
 
 export default ProductCatService;
--- src/features/product-category/ProductCatSlice.js
+++ src/features/product-category/ProductCatSlice.js
@@ -9,14 +9,29 @@ const initialState = {
   message: "",
 };
 
-export const getProCats = createAsyncThunk("brand/get-brands", async (thunkAPI) => {
-  try {
-    const response = await ProductCatService.getProCats();
-    return response;
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error.response.data);
+export const getProCats = createAsyncThunk(
+  "category/get-categories",
+  async (thunkAPI) => {
+    try {
+      const response = await ProductCatService.getProCats();
+      return response;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
   }
-});
+);
+
+export const addCategory = createAsyncThunk(
+  "category/add-category",
+  async (data, thunkAPI) => {
+    try {
+      const response = await ProductCatService.addCategory(data);
+      return response;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
+);
 
 export const productCatSlice = createSlice({
   name: "productCats",
@@ -38,8 +53,22 @@ export const productCatSlice = createSlice({
         state.message = action.payload.message;
         state.isError = true;
         state.isLoading = false;
+      })
+      .addCase(addCategory.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(addCategory.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isError = false;
+        state.isSuccess = true;
+        state.createPro = action.payload;
+      })
+      .addCase(addCategory.rejected, (state, action) => {
+        state.createPro = [];
+        state.message = action.payload.message;
+        state.isError = true;
+        state.isLoading = false;
       }),
 });
-
 
 export default productCatSlice.reducer;
